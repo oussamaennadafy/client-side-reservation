8101b749b1155d43c077d8b86adda267bc610967
oussama <oussama.ennadafy@gmail.com>
2022-11-14 09:58:22 +0100

    commit

---
+++ client/src/components/Register.js
@@ -0,0 +1,155 @@
+import React, { useContext, useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { adddata } from "./context/ContextProvider";
+
+const Register = () => {
+  const { udata, setUdata } = useContext(adddata);
+
+  const history = useHistory();
+
+  const [inpval, setINP] = useState({
+    departureCity: "",
+    arrivalCity: "",
+    departureTime: "",
+    arrivalTime: "",
+    seats: "",
+    price: "",
+  });
+
+  const setdata = (e) => {
+    const { name, value } = e.target;
+    setINP((preval) => {
+      return {
+        ...preval,
+        [name]: value,
+      };
+    });
+  };
+
+  const addinpdata = async (e) => {
+    e.preventDefault();
+
+    const {
+      departureCity,
+      arrivalCity,
+      departureTime,
+      arrivalTime,
+      seats,
+      price,
+    } = inpval;
+
+    const res = await fetch("http://localhost:5050/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        departureCity,
+        arrivalCity,
+        departureTime,
+        arrivalTime,
+        seats,
+        price,
+      }),
+    });
+
+    const data = await res.json();
+    if (res.status === 422 || !data) {
+      console.log("error ");
+      alert("error");
+    } else {
+      history.push("/");
+      setUdata(data);
+    }
+  };
+
+  return (
+    <div className="container">
+      <NavLink to="/">home</NavLink>
+      <form className="mt-4">
+        <div className="row">
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              departure City
+            </label>
+            <input
+              type="text"
+              value={inpval.departureCity}
+              onChange={setdata}
+              name="departureCity"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              arrivalCity
+            </label>
+            <input
+              type="text"
+              value={inpval.arrivalCity}
+              onChange={setdata}
+              name="arrivalCity"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              departureTime
+            </label>
+            <input
+              type="datetime-local"
+              value={inpval.departureTime}
+              onChange={setdata}
+              name="departureTime"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              arrival Time
+            </label>
+            <input
+              type="datetime-local"
+              value={inpval.arrivalTime}
+              onChange={setdata}
+              name="arrivalTime"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              seats
+            </label>
+            <input
+              type="number"
+              value={inpval.seats}
+              onChange={setdata}
+              name="seats"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Price
+            </label>
+            <input
+              type="number"
+              value={inpval.price}
+              onChange={setdata}
+              name="price"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" onClick={addinpdata} className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default Register;
