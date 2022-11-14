e47e1067eb2301a1791e2d8a0696a96231a39a0f
oussama <oussama.ennadafy@gmail.com>
2022-11-14 21:15:38 +0100

    updates

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Formik } from 'formik';
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import CreateIcon from "@mui/icons-material/Create";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
@@ -8,57 +9,85 @@ import { updatedata } from "./context/ContextProvider";
 
 const Home = () =>
 {
-  const [getuserdata, setUserdata] = useState([]);
-
-  const { udata, setUdata } = useContext(adddata);
-
-  const { updata, setUPdata } = useContext(updatedata);
-
-  const { dltdata, setDLTdata } = useContext(deldata);
-
-  const getdata = async () =>
+  const [data, setData] = useState([]);
+  const [formdata, setformdata] = useState(
+    {
+      departureCity: "",
+      arrivalCity: "",
+      departureDate: "",
+    }
+  );
+  const { departureCity, arrivalCity, departureDate } = formdata;
+  const onChange = (prevent) =>
   {
-    const res = await fetch("http://localhost:5050/getData", {
-      method: "GET",
+    setformdata({ ...formdata, [prevent.target.name]: prevent.target.value })
+  }
+  const search = async (e) =>
+  {
+    e.preventDefault();
+    document.getElementById("error").classList.add("hidden");
+    if (departureCity.trim() == "" || arrivalCity.trim() == "" || departureDate.trim() == "") {
+      document.getElementById("error").classList.remove("hidden");
+      console.log('plz fill');
+      return;
+    }
+    const res = await fetch("http://localhost:5051/search", {
+      method: "POST",
+      body: JSON.stringify(formdata),
       headers: {
         "Content-Type": "application/json",
       },
     });
 
     const data = await res.json();
+    if (!data.length) return window.alert("No Record Found");
+    const result = document.getElementById("result");
+    result.classList.remove("hidden");
+    result.scrollIntoView({ behavior: 'smooth' });
+    setData(data);
+  }
+  // const [getuserdata, setUserdata] = useState([]);
 
-    if (res.status === 422 || !data) {
-      console.log("error ");
-    } else {
-      setUserdata(data);
-    }
-  };
+  // const { udata, setUdata } = useContext(adddata);
 
-  useEffect(() =>
-  {
-    getdata();
-  }, []);
+  // const { updata, setUPdata } = useContext(updatedata);
 
-  const deletetrip = async (id) =>
-  {
-    const res2 = await fetch(`http://localhost:5050/deletetrip/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  // const { dltdata, setDLTdata } = useContext(deldata);
 
-    const deletedata = await res2.json();
-    console.log(deletedata);
+  // const departure_city = document.getElementById('departure_city')
+  // const arrival_city = document.getElementById('arrival_city')
+  // const departure_time = document.getElementById('departure_time')
+  // const arrival_time = document.getElementById('arrival_time')
+  // // const getdata = async () =>
+  // // {
+  // //   const res = await fetch("http://localhost:5050/getData", {
+  // //     method: "GET",
+  // //     headers: {
+  // //       "Content-Type": "application/json",
+  // //     },
+  // //   });
 
-    if (res2.status === 422 || !deletedata) {
-      console.log("error");
-    } else {
-      console.log("user deleted");
-      setDLTdata(deletedata);
-      getdata();
-    }
-  };
+  // //   const data = await res.json();
+
+  // //   if (res.status === 422 || !data) {
+  // //     console.log("error ");
+  // //   } else {
+  // //     setUserdata(data);
+  // //   }
+  // // };
+  // const search = async (e) =>
+  // {
+  //   e.preventDefault()
+  //   console.log(departure_city.value);
+  //   console.log(arrival_city.value);
+  //   console.log(departure_time.value);
+  //   console.log(arrival_time.value);
+  // };
+
+  // useEffect(() =>
+  // {
+  //   getdata();
+  // }, []);
 
   return (
     <>
@@ -120,33 +149,61 @@ const Home = () =>
       ) : (
         ""
       )} */}
-      <section className="h-[100vh-70px] flex justify-center" id="bg-pic">
-        <div className="text-white flex flex-col gap-5 pt-16 w-3/4 h-1/2 mt-16 relative rounded">
+      <section className="flex justify-center" id="bg-pic">
+        <div className="text-white flex flex-col gap-5 pt-16 w-3/4 h-1/2 mt-36 relative rounded">
           <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 rounded"></div>
           <div className="z-10 flex flex-col gap-5">
             <h1 className="text-center font-bold text-4xl">Find your next Trip</h1>
             <p className="text-center font-semibold text-xl">Search deals on hotels, homes, and much more...</p>
-            <form className="px-10 mt-5 h-20 flex gap-3">
-              <div className="flex w-1/6 flex-col gap-3">
+            <form className="px-10 mt-5 h-20 flex gap-3" onSubmit={search}>
+              <div className="flex w-1/4 flex-col gap-3">
                 <label>departure city</label>
-                <input className="h-1/2 text-gray-900 rounded outline-none px-2 py-1"></input>
+                <input className="h-1/2 text-gray-900 rounded outline-none px-2 py-1" onChange={onChange} id='departure_city' name="departureCity"></input>
               </div>
-              <div className="flex w-1/6 flex-col gap-3">
+              <div className="flex w-1/4 flex-col gap-3">
                 <label>arrival city</label>
-                <input className="h-1/2 text-gray-900 rounded outline-none px-2 py-1"></input>
+                <input onChange={onChange} id='arrivalCity' className="h-1/2 text-gray-900 rounded outline-none px-2 py-1" name="arrivalCity"></input>
               </div>
-              <div className="flex w-1/6 flex-col gap-3">
-                <label>departure time</label>
-                <input type="datetime-local" className="h-1/2 text-gray-900 rounded outline-none px-2 py-1"></input>
+              <div className="flex w-1/4 flex-col gap-3">
+                <label>departure date</label>
+                <input onChange={onChange} id='departure_date' type="date" className="h-1/2 text-gray-900 rounded outline-none px-2 py-1" name="departureDate"></input>
               </div>
-              <div className="flex w-1/6 flex-col gap-3">
-                <label>arrival time</label>
-                <input type="datetime-local" className="h-1/2 text-gray-900 rounded outline-none px-2 py-1"></input>
-              </div>
-              <button className="h-1/2 text-gray-100 bg-blue-400 rounded outline-none px-4 py-2 mt-auto mb-1 w-full">Search</button>
+              <button className="h-1/2 text-gray-100 bg-blue-500 rounded outline-none px-4 py-2 mt-auto mb-1 w-full" type="submit">Search</button>
             </form>
+            <div className="z-10 text-red-500 text-center -mt-4 hidden" id='error'>please fill all data inputs</div>
           </div>
         </div>
+      </section>
+      <section className="p-5 hidden min-h-screen bg-gray-50" id="result">
+        <h1 className="text-center font-bold text-4xl mb-12 mt-3">Result</h1>
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              {["departure City", "arrival City", "departure time", "arrival time", "seats", "price", "Booking"].map((item) => <th key={item} scope="col" className="py-3 px-6">{item}</th>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((element) =>
+            {
+              return (
+                <>
+                  <tr className="bg-white dark:bg-gray-800">
+                    <td className="py-4 px-6">{element.departureCity}</td>
+                    <td className="py-4 px-6">{element.arrivalCity}</td>
+                    <td className="py-4 px-6">{element.departureTime}</td>
+                    <td className="py-4 px-6">{element.arrivalTime}</td>
+                    <td className="py-4 px-6">{element.seats}</td>
+                    <td className="py-4 px-6">{element.price} Mad</td>
+                    <td className="py-4 px-6 text-green-400">
+                      <button className="bg-green-50 px-3 py-2 rounded font-me">Book Now</button>
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
       </section>
     </>
   );
--- client/src/components/Navbaar.js
+++ client/src/components/Navbaar.js
@@ -5,7 +5,7 @@ const Navbaar = () =>
 {
     return (
         <header>
-            <nav className="bg-[#353333] h-22 w-screen flex items-center justify-between">
+            <nav className="bg-[#353333] h-22 w-screen flex absolute items-center justify-between">
                 <div className='flex items-center'>
                     <img className='w-32 mb-5 cursor-pointer' src='https://www.oncf.ma/images/logo.png?v4.6'></img>
                     <ul className='flex gap-5 ml-20 text-gray-200'>
